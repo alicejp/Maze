804a440de2f2749e0db9453a6899b2e5310fcf7d
Alicejp <alail5077@gmail.com>
2021-06-24 18:03:33 +0100

    Add the first cool button

--- Assets/Scripts/DeadBody.cs
+++ Assets/Scripts/DeadBody.cs
@@ -4,27 +4,12 @@ using UnityEngine;
 
 public class DeadBody : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter(Collision other)
     {
         if (tag == "hasHint")
         {
-            Debug.Log("hasHint: " + gameObject.name);
             GetComponent<MeshRenderer>().material.color = Color.cyan;
             return;
         }
-        
-        Debug.Log("Do nothing");
     }
 }
